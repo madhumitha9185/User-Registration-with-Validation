ebd9a2f77cac9e961c84156f68d1761eddb300fd
madhumitha9185 <madhumitha9185@gmail.com>
2025-10-12 19:04:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-angular.module('registrationApp', [])
-.controller('RegistrationController', function($scope) {
-    $scope.user = {};
+var app = angular.module("userApp", []);
 
-    $scope.register = function() {
-        if ($scope.regForm.$valid && $scope.user.password === $scope.user.confirmPassword) {
-            alert('Registration successful!\n' + JSON.stringify($scope.user));
-        } else {
-            alert('Form is invalid!');
-        }
-    };
+app.controller("userCtrl", function($scope) {
+  $scope.user = {};
+  $scope.users = [];
+  $scope.successMessage = "";
+
+  $scope.submitForm = function() {
+    if ($scope.user.password === $scope.user.confirmPassword) {
+      // Save user info (name & email only)
+      $scope.users.push({
+        name: $scope.user.name,
+        email: $scope.user.email
+      });
+
+      console.log("User Registered Successfully");
+      console.log($scope.user);
+
+      $scope.successMessage = "User Registered Successfully!";
+      $scope.user = {};
+      $scope.regForm.$setPristine();
+      $scope.regForm.$setUntouched();
+    } else {
+      alert("Passwords do not match!");
+    }
+  };
 });
